ed11b3d4d173689bc7472e63b60c8d4a67191c91
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2021-03-03 09:11:17 -0800

    Finished Track
    Added a track. Will start working on track navigation

---
+++ Assets/Scripts/LapTracking/LapTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapTracker : MonoBehaviour
+{
+    [Header("Laps:")]
+    public int currentLap;
+    public int totalLaps;
+
+    [Header("Position In Lap:")]
+    public int positionInLap;
+    public int maxPositionInLap;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
