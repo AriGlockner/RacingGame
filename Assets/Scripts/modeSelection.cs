7ea02b79be790ccfd4f50950c060dd2eacc21e14
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2021-03-26 09:11:05 -0700

    fixed stuff

--- Assets/Scripts/modeSelection.cs
+++ Assets/Scripts/modeSelection.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class modeSelection : MonoBehaviour
 {
     [Header("Game Mode Selection Options:")]
     public string[] gameModes = new string[] { "freeplay", "beat the clock", "ghost computer", "live race against computer" };
     
-    public string gameModeSelected;
+    public static string gameModeSelected;
     public static int gameModeNumber;
+
+    [Header("Time:")]
+    public float timeTilStart;
+    public int timeInSeconds;
+    public Text textToDisplay;
+    public bool adjustTimeObject;
+
+    [Header("GameObjects")]
+    public GameObject car;
+    public Rigidbody rb;
+
+    public GameObject startTimeGameObject;
 
     void Awake()
     {
-        setMode();
+        startTimeGameObject = GameObject.FindGameObjectWithTag("initialCountDown");
+
+        car = GameObject.FindGameObjectWithTag("Player");
+        rb = car.GetComponent<Rigidbody>();
+        rb.Sleep();
+        adjustTimeObject = true;
     }
 
-    void Start()
+    void Update()
     {
-        //gameModes = new string[] { "freeplay", "beat the clock", "Ghost Computer", "Live Race Computer" };
+        if (adjustTimeObject)
+            startTime();
     }
 
     void setMode()
@@ -47,20 +66,100 @@ public class modeSelection : MonoBehaviour
     void startFreeplayMode()
     {
         Debug.Log("Freeplay mode selected!");
+
+        setGameObjectsWithTagState(true, false, false, false);
     }
 
     void startBeatTheClockMode()
     {
         Debug.Log("Beat the clock mode selected!");
+        timer.startCountDown = true;
+        setGameObjectsWithTagState(false, true, false, false);
     }
 
     void startGhostComputerMode()
     {
         Debug.Log("Race against a ghost mode selected!");
+
+        setGameObjectsWithTagState(false, false, true, false);
+        
     }
 
     void startAIRaceMode()
     {
         Debug.Log("Race against a live computer selected!");
+
+        setGameObjectsWithTagState(false, false, false, true);
+    }
+
+    void setGameObjectsWithTagState(bool isActive1, bool isActive2, bool isActive3, bool isActive4)
+    {
+        //Set GameObjects
+        GameObject[] fp;
+        fp = GameObject.FindGameObjectsWithTag("freeplay");
+        GameObject[] clock;
+        clock = GameObject.FindGameObjectsWithTag("raceAgainstClock");
+        GameObject[] ghost;
+        ghost = GameObject.FindGameObjectsWithTag("raceAgainstGhost");
+        GameObject[] computer;
+        computer = GameObject.FindGameObjectsWithTag("raceAgainstComputer");
+
+        //Freeplay GameObjects
+        foreach (GameObject obj in fp)
+        {
+            obj.SetActive(isActive1);
+        }
+
+        //Race Against the Clock GameObjects
+        foreach (GameObject obj in clock)
+        {
+            obj.SetActive(isActive2);
+        }
+
+        //Race Against the Ghost GameObjets
+        foreach (GameObject obj in ghost)
+        {
+            obj.SetActive(isActive3);
+        }
+
+        //Race Against the CPU GameObjects
+        foreach (GameObject obj in computer)
+        {
+            obj.SetActive(isActive4);
+        }
+    }
+
+    //Countdown to start the Game
+    void startTime()
+    {
+        timeTilStart -= Time.deltaTime;
+        timeInSeconds = (int)timeTilStart;
+
+        if (timeInSeconds > 4)
+        {
+            rb.Sleep();
+            textToDisplay.text = "";
+        }
+
+        else if (timeInSeconds > 0 && timeInSeconds < 4)
+        {
+            textToDisplay.text = timeInSeconds.ToString();
+            rb.Sleep();
+        }
+
+        else if (timeInSeconds == 0)
+        {
+            textToDisplay.text = "Go";
+            rb.WakeUp();
+
+            setMode();
+        }
+
+        else if (timeInSeconds < 0)
+        {
+            textToDisplay.text = "";
+            rb.WakeUp();
+            adjustTimeObject = false;
+        }
     }
 }
---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public float raceTime;
+    public int timeInSeconds;
+    public Text textToDisplay;
+    public static bool startCountDown;
+
+    void Start()
+    {
+        startCountDown = false;
+    }
+
+    void Update()
+    {
+        if (startCountDown)
+        {
+
+
+            raceTime -= Time.deltaTime;
+            timeInSeconds = (int)raceTime;
+
+            if (timeInSeconds < 0f)
+                SceneManager.LoadScene("LoseScreen");
+
+            textToDisplay.text = timeInSeconds.ToString();
+        }
+        else
+        {
+            textToDisplay.text = "";
+        }
+    }
+}
